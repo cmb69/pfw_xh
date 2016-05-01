84d5ab8d5ba073bf9ac27e5e157567e58cca36be
Christoph M. Becker <cmbecker69@gmx.de>
2016-05-01 15:14:03 +0200

    added GPLv3

--- classes/required_classes.php
+++ classes/required_classes.php
@@ -1,5 +1,12 @@
 <?php
 
+/**
+ * The autoloader
+ *
+ * @copyright 2016 Christoph M. Becker
+ * @license   http://www.gnu.org/licenses/gpl-3.0.en.html GPLv3
+ */
+
 spl_autoload_register(function ($className) {
     $parts = explode('\\', $className);
     $parts[0] = lcfirst($parts[0]);
