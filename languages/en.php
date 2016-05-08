cda0249a00adea4f338e855508f5d76b1526f2a1
Christoph M. Becker <cmbecker69@gmx.de>
2016-05-08 11:49:32 +0200

    add validation error messages and rudimentary styling

--- languages/en.php
+++ languages/en.php
@@ -9,3 +9,10 @@ $plugin_tx['pfw']['syscheck_writable']="%s is writable";
 $plugin_tx['pfw']['syscheck_alt_success']="Success";
 $plugin_tx['pfw']['syscheck_alt_warning']="Warning";
 $plugin_tx['pfw']['syscheck_alt_failure']="Failure";
+
+$plugin_tx['pfw']['validation_minlength']="%s must be at least %d characters long!";
+$plugin_tx['pfw']['validation_maxlength']="%s must be at most %d characters long!";
+$plugin_tx['pfw']['validation_required']="%s is required!";
+$plugin_tx['pfw']['validation_pattern']="%s must match %s!";
+$plugin_tx['pfw']['validation_min']="%s must be at least %f!";
+$plugin_tx['pfw']['validation_max']="%s must be at most %f!";
