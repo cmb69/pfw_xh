5c9907f21b18c214756dd6bd06e394ff42928080
Christoph M. Becker <cmbecker69@gmx.de>
2016-05-08 13:13:28 +0200

    views are inherited from the plugin framework

--- classes/View.php
+++ classes/View.php
@@ -36,6 +36,10 @@ class View
 
     private function templatePath()
     {
-        return $this->plugin->folder() . "views/{$this->template}.php";
+        $filename = $this->plugin->folder() . "views/{$this->template}.php";
+        if (file_exists($filename)) {
+            return $filename;
+        }
+        return $this->plugin->folder() . "../pfw/views/{$this->template}.php";
     }
 }
