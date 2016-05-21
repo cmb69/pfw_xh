015ebd513b1899d55b403ed848f8db0a21d25cbe
Christoph M. Becker <cmbecker69@gmx.de>
2016-05-21 13:09:31 +0200

    Get rid of phpunit/phpunit-dom-assertions
    This package seems to be unmaintained, and we currently don't use it
    anyway.

--- tests/unit/FormTest.php
+++ tests/unit/FormTest.php
@@ -21,7 +21,7 @@ along with Pfw_XH.  If not, see <http://www.gnu.org/licenses/>.
 
 namespace Pfw;
 
-class FormTest extends \PHPUnit_Framework_DOMTestCase
+class FormTest extends \PHPUnit_Framework_TestCase
 {
     private $subject;
 
