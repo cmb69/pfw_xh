aa776aa9a2a42d426fd1a78821b2e998098c7003
Christoph M. Becker <cmbecker69@gmx.de>
2016-05-22 02:23:12 +0200

    Use short PHP echo tags in templates
    This appears to be more readable, and we cater to PHP 5.3, where this
    feature might not be available, while building the distributable.

--- views/info.php
+++ views/info.php
@@ -1,7 +1,7 @@
-<h1><?php echo $this->escape($title)?></h1>
-<img class="pfw_logo" src="<?php echo $this->escape($logo)?>" alt="<?php echo $this->text('alt_logo')?>"/>
-<p>Version <?php echo $this->escape($plugin->version)?></p>
-<p>Copyright <?php echo $this->escape($plugin->copyright)?></p>
+<h1><?=$this->escape($title)?></h1>
+<img class="pfw_logo" src="<?=$this->escape($logo)?>" alt="<?=$this->text('alt_logo')?>"/>
+<p>Version <?=$this->escape($plugin->version)?></p>
+<p>Copyright <?=$this->escape($plugin->copyright)?></p>
 <p class="pfw_license">This program is free software: you can redistribute it
 and/or modify it under the terms of the GNU General Public License as published
 by the Free Software Foundation, either version 3 of the License, or (at your
@@ -13,12 +13,12 @@ GNU General Public License for more details.</p>
 <p class="pfw_license">You should have received a copy of the GNU General
 Public License along with this program. If not, see <a
 href="http://www.gnu.org/licenses/">http://www.gnu.org/licenses/</a>.</p>
-<h4><?php echo $this->text('syscheck_title')?></h4>
-<?php echo $systemCheck->render()?>
-<h4><?php echo $this->text('userfunc_title')?></h4>
+<h4><?=$this->text('syscheck_title')?></h4>
+<?=$systemCheck->render()?>
+<h4><?=$this->text('userfunc_title')?></h4>
 <dl class="pfw_userfuncs">
 <?php foreach ($plugin->getFuncNames() as $funcName):?>
-    <dt><?php echo $this->escape($userFuncSignature($funcName))?></dt>
-    <dd><?php echo $this->text("userfunc_$funcName")?></dd>
+    <dt><?=$this->escape($userFuncSignature($funcName))?></dt>
+    <dd><?=$this->text("userfunc_$funcName")?></dd>
 <?php endforeach?>
 </dl>
