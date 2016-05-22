59f52981406e11e48202b4f3cfe4e0df92146287
Christoph M. Becker <cmbecker69@gmx.de>
2016-05-22 12:39:44 +0200

    Add sections to info view
    No need to cater to HTML 4 any more.

--- views/info.php
+++ views/info.php
@@ -1,24 +1,36 @@
-<h1><?=$this->escape($title)?></h1>
-<img class="pfw_logo" src="<?=$this->escape($logo)?>" alt="<?=$this->text('alt_logo')?>"/>
-<p>Version <?=$this->escape($plugin->version)?></p>
-<p>Copyright <?=$this->escape($plugin->copyright)?></p>
-<p class="pfw_license">This program is free software: you can redistribute it
-and/or modify it under the terms of the GNU General Public License as published
-by the Free Software Foundation, either version 3 of the License, or (at your
-option) any later version.</p>
-<p class="pfw_license">This program is distributed in the hope that it will be
-useful, but <em>without any warranty</em>; without even the implied warranty of
-<em>merchantability</em> or <em>fitness for a particular purpose</em>. See the
-GNU General Public License for more details.</p>
-<p class="pfw_license">You should have received a copy of the GNU General
-Public License along with this program. If not, see <a
-href="http://www.gnu.org/licenses/">http://www.gnu.org/licenses/</a>.</p>
-<h4><?=$this->text('syscheck_title')?></h4>
-<?=$systemCheck->render()?>
-<h4><?=$this->text('userfunc_title')?></h4>
-<dl class="pfw_userfuncs">
+<section class="pfw_about">
+    <h1><?=$this->escape($title)?></h1>
+    <img class="pfw_logo" src="<?=$this->escape($logo)?>" alt="<?=$this->text('alt_logo')?>"/>
+    <p>Version <?=$this->escape($plugin->version)?></p>
+    <p>Copyright <?=$this->escape($plugin->copyright)?></p>
+    <p class="pfw_license">
+        This program is free software: you can redistribute it
+        and/or modify it under the terms of the GNU General Public License as published
+        by the Free Software Foundation, either version 3 of the License, or (at your
+        option) any later version.
+    </p>
+    <p class="pfw_license">
+        This program is distributed in the hope that it will be
+        useful, but <em>without any warranty</em>; without even the implied warranty of
+        <em>merchantability</em> or <em>fitness for a particular purpose</em>. See the
+        GNU General Public License for more details.
+    </p>
+    <p class="pfw_license">
+        You should have received a copy of the GNU General
+        Public License along with this program. If not, see <a
+        href="http://www.gnu.org/licenses/">http://www.gnu.org/licenses/</a>.
+    </p>
+    <section class="pfw_syscheck">
+        <h4><?=$this->text('syscheck_title')?></h4>
+        <?=$systemCheck->render()?>
+    </section>
+    <section class="pfw_userfuncs">
+        <h4><?=$this->text('userfunc_title')?></h4>
+        <dl class="pfw_userfuncs">
 <?php foreach ($plugin->getFuncNames() as $funcName):?>
-    <dt><?=$this->escape($userFuncSignature($funcName))?></dt>
-    <dd><?=$this->text("userfunc_$funcName")?></dd>
+            <dt><?=$this->escape($userFuncSignature($funcName))?></dt>
+            <dd><?=$this->text("userfunc_$funcName")?></dd>
 <?php endforeach?>
-</dl>
+        </dl>
+    </section>
+</section>
