cmb69/pfw_xh

commit e807350d8cef3a2ea092537e8401d22302b5e4b5

Close #3: Use "new style" push views
Being able to push closures to the view has disadvantages, namely that
the view is easily inclined to cache the results of calling some
closures, and even worse, that escaping by convention doesn't work in
all cases, for instance, when passing a closure result (which has
already been escaped) as additional argument to ::text(), which
absolutely has to escape its return value.

Therefore we're switching to pure push views, i.e. it's not allowed
anymore to push closures and call them from the view. This gives us the
option to to overload __call for view data (which triggers escaping),
but let __get access unescaped. Thus, a simple convention should be
sufficient to avoid XSS issues: only simple (i.e. non nested) method
calls on $this should be echo'd.

The drawback of having to construct sometimes very complex DTOs,
appears to be outweighed by the mentioned advantages. Furthermore, a
simple var_dump($this) in the view will show *all* available data.

Finally, we proclaim that views should only use a *very* limited subset
of PHP. It's not absolutely clear to us which exact subset, but for now
we want to use only `foreach` and `if` statements (besides implicit
`echo` via <?=), and variable, member access, dynamic method call and
`empty` expressions. Currently, we're also using string interpolation
which might not be the best idea.

views/info.php
@@ -1,8 +1,8 @@
 <section class="pfw_about">
-    <h1><?=$this->title?></h1>
-    <img class="pfw_logo" src="<?=$this->logo?>" alt="<?=$this->text('alt_logo')?>"/>
-    <p>Version <?=$this->escape($model->version())?></p>
-    <p>Copyright <?=$this->escape($model->copyright())?></p>
+    <h1><?=$this->title()?></h1>
+    <img class="pfw_logo" src="<?=$this->logo()?>" alt="<?=$this->text('alt_logo')?>"/>
+    <p>Version <?=$this->escape($this->model->version())?></p>
+    <p>Copyright <?=$this->escape($this->model->copyright())?></p>
     <p class="pfw_license">
         This program is free software: you can redistribute it
         and/or modify it under the terms of the GNU General Public License as published
@@ -23,10 +23,10 @@
     <section class="pfw_syscheck">
         <h4><?=$this->text('syscheck_title')?></h4>
         <ul class="pfw_syscheck">
-<?php foreach ($systemCheck->checks() as $check):?>
+<?php foreach ($this->checks as $check):?>
             <li>
-                <img src="<?=$this->statusIcon($check)?>" alt="<?=$this->text($statusAlt($check))?>">
-                <?=$this->escape($check->text())?>
+                <img src="<?=$this->escape($check->statusIcon)?>" alt="<?=$this->text($check->statusAlt)?>">
+                <?=$this->escape($check->text)?>
             </li>
 <?php endforeach?>
         </ul>
@@ -34,9 +34,9 @@
     <section class="pfw_userfuncs">
         <h4><?=$this->text('userfunc_title')?></h4>
         <dl class="pfw_userfuncs">
-<?php foreach ($model->getFuncNames() as $funcName):?>
-            <dt><?=$this->userFuncSignature($funcName)?></dt>
-            <dd><?=$this->text("userfunc_$funcName")?></dd>
+<?php foreach ($this->userFuncs as $userFunc):?>
+            <dt><?=$this->escape($userFunc->name)?></dt>
+            <dd><?=$this->text("userfunc_{$userFunc->signature}")?></dd>
 <?php endforeach?>
         </dl>
     </section>

classes/HtmlString.php
@@ -40,7 +40,7 @@ class HtmlString
      */
     public function __construct($contents)
     {
-        $this->contents = $contents;
+        $this->contents = (string) $contents;
     }
 
     /**

classes/HtmlView.php
@@ -29,15 +29,14 @@ class HtmlView extends View
     /**
      * Returns a properly HTML escaped string.
      *
-     * @param string $string
-     *
+     * @param mixed $value
      * @return string
      */
-    protected function escape($string)
+    protected function escape($value)
     {
-        if ($string instanceof HtmlString) {
-            return (string) $string;
+        if ($value instanceof HtmlString) {
+            return (string) $value;
         }
-        return htmlspecialchars($string, ENT_COMPAT, 'UTF-8');
+        return htmlspecialchars($value, ENT_COMPAT, 'UTF-8');
     }
 }

classes/PluginInfoController.php
@@ -44,15 +44,17 @@ class PluginInfoController extends Controller
         $view->model = $this->plugin;
         $view->title = $title;
         $view->logo = $this->plugin->folder() . $this->plugin->name() . '.png';
-        $view->systemCheck = $this->systemCheck();
+        $view->checks = array_map(function ($check) {
+            return (object) array(
+                'text' => $check->text(),
+                'statusIcon' => $this->plugin->folder() . 'images/' . $check->status() . '.png',
+                'statusAlt' => 'syscheck_alt_' . $check->status()
+            );
+        }, $this->systemCheck()->checks());
         $plugin = $this->plugin;
-        $view->statusIcon = function ($check) {
-            return $this->plugin->folder() . 'images/' . $check->status() . '.png';
-        };
-        $view->statusAlt = function ($check) {
-            return 'syscheck_alt_' . $check->status();
-        };
-        $view->userFuncSignature = $this->userFuncSignatureFunc();
+        $view->userFuncs = array_map(function ($funcName) {
+            return (object) ['name' => $funcName, 'signature' => $this->userFuncSignature($funcName)];
+        }, $this->plugin->getFuncNames());
         $view->render();
     }
     
@@ -72,15 +74,17 @@ class PluginInfoController extends Controller
                 ->extension('XMLWriter')
                 ->xhVersion('1.6');
     }
-    
-    private function userFuncSignatureFunc()
+
+    /**
+     * @param string $functionName
+     * @return string
+     */
+    private function userFuncSignature($functionName)
     {
-        return function ($functionName) {
-            $params = [];
-            foreach ($this->plugin->funcParams($functionName) as $param) {
-                $params[] = $param->getName();
-            }
-            return sprintf('%s(%s)', $functionName, implode(', ', $params));
-        };
+        $params = [];
+        foreach ($this->plugin->funcParams($functionName) as $param) {
+            $params[] = $param->getName();
+        }
+        return sprintf('%s(%s)', $functionName, implode(', ', $params));
     }
 }

classes/View.php
@@ -27,26 +27,23 @@ namespace Pfw;
  * Views are helper objects that render a given PHP template, i.e. `echo` it.
  * The template usually contains PHP tags, which preferably are constrained
  * to simple loops and `echo` statements. To keep logic out of the templates,
- * the controller sets arbitrary view properties, which can have values of any
- * type; notably, callables are supported.
+ * the controller sets arbitrary view properties.
  *
- * These properties are available in the template as local variables,
- * and additionally as properties and methods, respectively, in which case they
- * yield properly escaped strings according to the view class.
+ * These properties are available in the template as properties of `$this`,
+ * and additionally as methods, in which case they
+ * return properly escaped strings according to the view class.
  * Note, that all real view methods also return escaped strings.
  *
- * While the template has access to all private class members, this is
- * discouraged. Instead only the protected and public members and the local
- * variables should be used.
+ * While the template has access to all private class members, using these is
+ * discouraged. Instead only the protected and public members should be used.
  *
  * To avoid XSS and garbled output (such as unescaped < in HTML) everything
  * that's `echo`'d from the template has to be properly escaped.
- * A simple convention supports this requirement: only `echo` view properties
- * and the results of calling view methods (as both are already escaped), i.e.
- * after each `echo` there should be a `$this->`.
+ * A simple convention supports this requirement: only `echo`
+ * the results of calling view methods (as these are already escaped).
  * To avoid escaping of strings containing HTML, use HtmlString.
- * Use the local variables when you don't `echo` (e.g. to iterate over them
- * with a foreach loop), or explicitly pass them to View::escape().
+ * Use the properties when you don't `echo` (e.g. to iterate over them
+ * with a foreach loop).
  */
 class View
 {
@@ -81,8 +78,6 @@ class View
     /**
      * The store for the supplied properties
      *
-     * This are available in the template as local variables.
-     *
      * @var array
      *
      * @see __set()
@@ -105,11 +100,10 @@ class View
     }
     
     /**
-     * Allows to set data and callbacks as properties of the view.
+     * Allows to set data as properties and methods of the view.
      *
-     * These properties are available in the template as local variables,
-     * as well as properties of the view.  Accessing as view properties
-     * automagically escapes the (return) values.
+     * These data are available as properties and methods of the view.
+     * Accessing them as view methods automagically escapes the values.
      *
      * As __set() will be triggered from outside the view, any valid PHP
      * identifier would be accepted as $name.  However, the template may
@@ -124,12 +118,7 @@ class View
      */
     public function __set($name, $value)
     {
-        if (is_callable($value)) {
-            $forbidden = method_exists($this, $name);
-        } else {
-            $forbidden = property_exists($this, $name);
-        }
-        if ($forbidden) {
+        if (property_exists($this, $name) || method_exists($this, $name)) {
             trigger_error("property $name not allowed", E_USER_WARNING);
             return;
         }
@@ -137,44 +126,50 @@ class View
     }
 
     /**
-     * Allows to retrieve previously set data as view property.
-     *
-     * The data will be escaped.
+     * Checks whether a certain data item has been assigned and is not null.
      *
      * @param string $name
+     * @return bool
+     */
+    public function __isset($name)
+    {
+        return isset($this->data[$name]);
+    }
+
+    /**
+     * Allows to retrieve previously set data as view property.
      *
-     * @return string
+     * The data will be returned as set.
+     *
+     * @param string $name
+     * @return mixed
      */
     public function __get($name)
+    {
+        return $this->data[$name];
+    }
+
+    /**
+     * Allows to retrieve previously set data as view method.
+     *
+     * The data value will be escaped.
+     *
+     * @param string $name
+     * @return string
+     */
+    public function __call($name, $args)
     {
         return $this->escape($this->data[$name]);
     }
 
     /**
-     * Allows to call previously set callbacks as view methods.
-     *
-     * The return value will be escaped.
-     *
-     * @param string $name
-     * @param array  ...$args
-     *
-     * @return string
-     */
-    public function __call($name, $args)
-    {
-        return $this->escape(call_user_func_array($this->data[$name], $args));
-    }
-
-    /**
      * Returns an escaped language string
      *
-     * Additional paramters are processed in an sprintf style.
+     * Additional parameters are processed in an sprintf style.
      *
      * @param string $key
      * @param array  ...$args
-     *
      * @return string
-     *
      * @SuppressWarnings(PHPMD.UnusedFormalParameter)
      */
     protected function text($key)
@@ -187,14 +182,12 @@ class View
     /**
      * Returns an escaped pluralized language string
      *
-     * Additional paramters are processed in an sprintf style.
+     * Additional parameters are processed in an sprintf style.
      *
      * @param string $key
      * @param int    $count
      * @param array  ...$args
-     *
      * @return string
-     *
      * @SuppressWarnings(PHPMD.UnusedFormalParameter)
      */
     protected function plural($key, $count)
@@ -211,7 +204,6 @@ class View
      */
     public function render()
     {
-        extract($this->data);
         include $this->templatePath();
     }
 
@@ -251,7 +243,6 @@ class View
      * This base implementation simply returns the string as is.
      *
      * @param string $string
-     *
      * @return string
      */
     protected function escape($string)
@@ -265,7 +256,7 @@ class View
      *
      * Actually, this is just a convenience wrapper for Controller::url().
      * More complex cases (such as setting additional query
-     * parameters) would require to pass a respective function to the view
+     * parameters) would require to pass an appropriate URL to the view
      * from the controller.
      *
      * @param string $action

tests/unit/HtmlViewTest.php
@@ -68,15 +68,23 @@ class HtmlViewTest extends TestCase
     
     public function testEscapesStrings()
     {
-        $this->setUpTemplate('<?php echo $this->escape($foo)? >');
+        $this->setUpTemplate('<?php echo $this->escape($this->foo)? >');
         $this->subject->foo = '<"&>';
         $this->expectOutputString('&lt;&quot;&amp;&gt;');
         $this->subject->render();
     }
 
-    public function testAutoEscapesProperties()
+    public function testDoesNotEscapeProperties()
     {
         $this->setUpTemplate('<?php echo $this->foo? >');
+        $this->subject->foo = '<"&>';
+        $this->expectOutputString('<"&>');
+        $this->subject->render();
+    }
+
+    public function testAutoEscapesMethods()
+    {
+        $this->setUpTemplate('<?php echo $this->foo()? >');
         $this->subject->foo = '<"&>';
         $this->expectOutputString('&lt;&quot;&amp;&gt;');
         $this->subject->render();
@@ -84,32 +92,12 @@ class HtmlViewTest extends TestCase
 
     public function testDoesNotEscapeHtmlStrings()
     {
-        $this->setUpTemplate('<?php echo $this->escape($foo)? >');
+        $this->setUpTemplate('<?php echo $this->escape($this->foo)? >');
         $this->subject->foo = new HtmlString('<"&>');
         $this->expectOutputString('<"&>');
         $this->subject->render();
     }
     
-    public function testEscapesCallbacks()
-    {
-        $this->setUpTemplate('<?php echo $this->escape($foo())? >');
-        $this->subject->foo = function () {
-            return '<"&>';
-        };
-        $this->expectOutputString('&lt;&quot;&amp;&gt;');
-        $this->subject->render();
-    }
-
-    public function testAutoEscapesCallbackProperties()
-    {
-        $this->setUpTemplate('<?php echo $this->foo()? >');
-        $this->subject->foo = function () {
-            return '<"&>';
-        };
-        $this->expectOutputString('&lt;&quot;&amp;&gt;');
-        $this->subject->render();
-    }
-
     private function setUpTemplate($contents)
     {
         $viewFolder = $this->root->url() . DIRECTORY_SEPARATOR . 'views';

commit 888f1894c9688e3ab6f92a62d122e8f12c2cfa8f

Fix display of userfuncs in plugin info
This has been broken by commit e807350, where we mixed up name and
signature.

views/info.php
@@ -37,8 +37,8 @@
         <h4><?=$this->text('userfunc_title')?></h4>
         <dl class="pfw_userfuncs">
 <?php   foreach ($this->userFuncs as $userFunc):?>
-            <dt><?=$this->escape($userFunc->name)?></dt>
-            <dd><?=$this->text("userfunc_{$userFunc->signature}")?></dd>
+            <dt><?=$this->escape($userFunc->signature)?></dt>
+            <dd><?=$this->text("userfunc_{$userFunc->name}")?></dd>
 <?php   endforeach?>
         </dl>
     </section>