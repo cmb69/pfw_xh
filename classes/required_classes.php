501464fd88ac6f0f7855a370cecbecd80e0834ce
Christoph M. Becker <cmbecker69@gmx.de>
2017-08-05 13:07:00 +0200

    Add general class autoloader

---
+++ classes/required_classes.php
@@ -0,0 +1,31 @@
+<?php
+
+/*
+ * Copyright 2016-2017 Christoph M. Becker
+ *
+ * This file is part of Pfw_XH.
+ *
+ * Pfw_XH is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Pfw_XH is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Pfw_XH.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+spl_autoload_register(function ($className) {
+    $parts = explode('\\', $className);
+    $parts[0] = lcfirst($parts[0]);
+    array_splice($parts, 1, 0, ['classes']);
+    array_unshift($parts, dirname(dirname(__DIR__)));
+    $classFile = implode(DIRECTORY_SEPARATOR, $parts) . '.php';
+    if (file_exists($classFile)) {
+        include_once $classFile;
+    }
+});
